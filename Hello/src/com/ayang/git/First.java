e48c0980c605dc128fe503334f6fcedfbfc32ff4
553163076 <553163076@qq.com>
2020-08-11 15:07:24 +0800

    把本地工程添加到git

---
+++ Hello/src/com/ayang/git/First.java
@@ -0,0 +1,18 @@
+package com.ayang.git;
+
+/**
+ * @Author: Yang
+ * @Description:
+ * @Date: Created in 14:56 2020/8/11
+ * @Modified By:
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+
+    }
+}
